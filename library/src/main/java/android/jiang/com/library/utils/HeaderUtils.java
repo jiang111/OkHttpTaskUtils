013f871786a863ed46e31e65f040ebb809d1ac61
jiang <425698907@qq.com>
2016-03-31 10:29:14 +0800

    add jcenter

---
+++ library/src/main/java/android/jiang/com/library/utils/HeaderUtils.java
@@ -0,0 +1,62 @@
+/**
+ * created by jiang, 10/23/15
+ * Copyright (c) 2015, jyuesong@gmail.com All Rights Reserved.
+ * *                #                                                   #
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG              #
+ * #                                                   #
+ */
+
+package android.jiang.com.library.utils;
+
+
+import android.jiang.com.library.Param;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * Created by jiang on 10/23/15.
+ */
+public class HeaderUtils {
+
+    public static final String HEADER_KEY = "k12AV";
+    public static final String HEADER_VALUE = "v2";
+
+
+    public static List<Param> validateHeaders(Map<String, String> header) {
+        if (header == null || header.size() == 0)
+            return null;
+        List<Param> results = new LinkedList<>();
+        Set<Map.Entry<String, String>> entrySet = header.entrySet();
+        for (Map.Entry<String, String> entry : entrySet) {
+            Param param = new Param(entry.getKey(), entry.getValue());
+            results.add(param);
+        }
+        return results;
+
+
+    }
+}
---
+++ library/src/main/java/android/jiang/com/library/ws_ret.java
@@ -0,0 +1,78 @@
+/**
+ * created by jiang, 15/10/19
+ * Copyright (c) 2015, jyuesong@gmail.com All Rights Reserved.
+ * *                #                                                   #
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG              #
+ * #                                                   #
+ */
+package android.jiang.com.library;
+
+import android.support.annotation.IntDef;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 网络请求失败或者没数据的时候，调用的类
+ * Created by jiang on 15/10/16.
+ */
+public class ws_ret {
+    @SerializedName("status")
+    @Expose
+    private int state;
+    @Expose
+    private String msg;
+
+    public ws_ret() {
+    }
+
+    public ws_ret(@STATE int state, String msg) {
+        this.state = state;
+        this.msg = msg;
+    }
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(@STATE int state) {
+        this.state = state;
+    }
+
+    @IntDef({WS_State.SUCCESS, WS_State.NONET, WS_State.NODATA, WS_State.OTHERS})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface STATE {
+    }
+}
